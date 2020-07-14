248198fd5a9fd75f203043d2d91f53161b50f1be
MikuPy2001 <794126318@qq.com>
2020-07-14 18:01:53 +0800

    12345

--- foldersEditControls.py
+++ foldersEditControls.py
@@ -1,21 +1,90 @@
+import traceback
+import types
 import imghdr
 import os
-
+import json
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class MyQListWidgetItem(QtWidgets.QListWidgetItem):
-    def __init__(self):
-        super().__init__()
-        self.setFlags(
-            QtCore.Qt.ItemIsEnabled |
-            QtCore.Qt.ItemIsEditable)
+def getItem(Name, Path, ImagePath, WIP):
+    item = QtWidgets.QListWidgetIte()
+    item.setText(Name)
+    item.Path = Path
+    item.ImagePath = ImagePath
+    item.WIP = WIP
+    return item
 
 
-class MyQListWidget(QtWidgets.QListWidget):
+# class MyQListWidgetItem(QtWidgets.QListWidgetItem):
+#     def __init__(self):
+#         super().__init__()
+#         print("项目初始化")
+
+
+class MyQListWidget1(QtWidgets.QListWidget):  # 解决item拖放问题
+    # dropFormat="application/x-MyQListWidget1List"
+    dropFormat = "application/x-qabstractitemmodeldatalist"
+
+    def mimeData(self, items):  # 拖出序列化
+        j = []
+        for item in items:
+            j.append([item.text(), item.Path, item.ImagePath, item.WIP])
+        jsonstr = json.dumps(j)
+        jsongbyte = jsonstr.encode("utf-8")
+        data = QtCore.QMimeData()
+        data.setData(self.dropFormat,
+                     QtCore.QByteArray(jsongbyte))
+        return data
+
+    def dropMimeData(self, index, data, action):  # 插入序列化
+        if not data.hasFormat(self.dropFormat):
+            return super().dropMimeData(index, data, action)
+        # print("自己插入的")
+        jsonQByteArray = data.data(self.dropFormat)
+        jsonbyte = jsonQByteArray.data()
+        jsonstr = str(jsonbyte, encoding="utf-8")
+        itemlist = json.loads(jsonstr)
+        for itemstrs in itemlist:
+            self.insertItem(index, getItem(
+                itemstrs[0],
+                itemstrs[1],
+                itemstrs[2],
+                itemstrs[3]
+            ))
+            index += 1
+
+        return True
+
+    # def dragEnterEvent(self, e):  # 进入
+    #     super().dragEnterEvent(e)
+    #     if e.mimeData().hasFormat(self.dropFormat):
+    #         e.acceptProposedAction()
+
+    # def dragMoveEvent(self, e):  # 移动
+    #     super().dragMoveEvent(e)
+    #     if e.mimeData().hasFormat(self.dropFormat):
+    #         e.acceptProposedAction()
+
+
+class MyQListWidget2(MyQListWidget1):  # 双击编辑item
+    def __init__(self, Form):
+        super().__init__(Form)
+        self.itemDoubleClicked.connect(self.itemDoubleClickedEVE)
+
+    def itemDoubleClickedEVE(self, item):
+        # self.openPersistentEditor(item)
+        item.setFlags(
+            item.flags() |
+            QtCore.Qt.ItemIsEditable
+        )
+
+
+class MyQListWidget(MyQListWidget2):  # 接受 文件拖入
+
     def __init__(self, Form):
         super().__init__(Form)
         self.isDragEnter = False
+        self.itemDoubleClicked.connect(self.itemDoubleClickedEVE)
 
     def dragEnterEvent(self, e):  # 进入
         super().dragEnterEvent(e)
@@ -53,8 +122,13 @@ class MyQListWidget(QtWidgets.QListWidget):
 class MyQLabel(QtWidgets.QLabel):
     imgType_list = ['jpg', 'bmp', 'png', 'jpeg', 'rgb', 'tif']
 
+    def __init__(self, Form):
+        super().__init__(Form)
+        self.imgloc = ""
+
     def dragEnterEvent(self, e):  # 进入
         super().dragEnterEvent(e)
+        # TODO 检查拖放类型
         path = e.mimeData().text().replace('file:///', '')
         paths = path.splitlines()
         if len(paths) > 1:
@@ -63,6 +137,9 @@ class MyQLabel(QtWidgets.QLabel):
         imgType = imghdr.what(path)
         if imgType in self.imgType_list:  # 受支持的格式
             self.isDragEnter = True
+            # self.pixmapbak = self.pixmap
+            # print(self.pixmapbak)
+            # self.setText("松开鼠标以设置封面")
             e.acceptProposedAction()
         else:
             print("格式不受支持:"+imgType)
@@ -84,5 +161,10 @@ class MyQLabel(QtWidgets.QLabel):
         self.isDragEnter = False
         path = e.mimeData().text().replace('file:///', '')  # 删除多余开头
 
-        pix = QtGui.QPixmap(path)
-        self.setPixmap(pix)
+        self.imgloc = path
+
+    def refreshImg(self):
+        imgType = imghdr.what(self.imgloc)
+        if imgType in self.imgType_list:  # 受支持的格式
+            pix = QtGui.QPixmap(self.imgloc)
+            self.setPixmap(pix)
--- foldersEditLauncher.py
+++ foldersEditLauncher.py
@@ -1,19 +1,38 @@
+import ctypes
 import sys
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-from 歌曲文件夹 import Ui_Form
+from foldersEdit import Ui_Form
+import foldersEditControls
+
+import os
+from os import path
+from xml.dom.minidom import parse
+
+
+def getNodeData(xml, TagName, default=""):
+    Nodes = xml.getElementsByTagName(TagName)
+    if len(Nodes) > 0:
+        return Nodes[0].firstChild.data
+    else:
+        return default
 
 
 class 窗口事件处理(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
-        self.无用代码()
+        # self.无用代码()
+        self.loadxml()
 
     def initUI(self):
         self.ui = Ui_Form()
         self.ui.setupUi(self)
+        # 这是为了解决window下不能设置任务栏图标的bug
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(
+            "foldersEdit")
+        # 设置图片
         self.setWindowIcon(QtGui.QIcon('Icon.png'))
         # 禁止最大化按钮
         self.setWindowFlags(
@@ -22,18 +41,58 @@ class 窗口事件处理(QtWidgets.QWidget):
         # 禁止拉伸窗口大小
         self.setFixedSize(self.width(), self.height())
 
+        # 关联点击事件,单击即可开始编辑
+        self.ui.listWidget0.itemClicked.connect(self.listItemClickedEVE)
+        self.ui.listWidget1.itemClicked.connect(self.listItemClickedEVE)
+        self.ui.listWidget2.itemClicked.connect(self.listItemClickedEVE)
+        self.ui.listWidgetDel.itemClicked.connect(self.listItemClickedEVE)
+        self.nowitem = None
+
         self.show()
+
+    def listItemClickedEVE(self, item):
+        # TODO
+        if isinstance(item, foldersEditControls.MyQListWidgetItem):
+            print(("被单击", type(item), item.text(),
+                   item.WIP, item.Path, item.ImagePath))
+        else:
+            print(("被单击", type(item), item.text()))
+
+    def loadxml(self):
+        foldersfile = path.join(os.getcwd(), 'Beat Saber', 'UserData',
+                                'SongCore', 'folders (2).xml')
+        if not path.isfile(foldersfile):  # 没有自定义目录
+            return
+        folders = parse(
+            foldersfile).documentElement.getElementsByTagName("folder")
+        print(("一共有", len(folders), "个目录被读取"))
+        for folder in folders:
+            item = foldersEditControls.getItem(
+                getNodeData(folder, 'Name'),
+                getNodeData(folder, 'Path'),
+                getNodeData(folder, 'ImagePath'),
+                getNodeData(folder, 'WIP').lower() == 'true'
+            )
+            Pack = getNodeData(folder, 'Pack')
+            if Pack == '0':
+                self.ui.listWidget0.addItem(item)
+            elif Pack == '1':
+                self.ui.listWidget1.addItem(item)
+            elif Pack == '2':
+                self.ui.listWidget2.addItem(item)
+            else:
+                self.ui.listWidgetDel.addItem(item)
 
     def 无用代码(self):
         # 无用代码
         for i in range(10):
             item = QtWidgets.QListWidgetItem()
             item.setText(f"默认-{i}")
-            self.ui.listWidget1.addItem(item)
+            self.ui.listWidget0.addItem(item)
         for i in range(10):
             item = QtWidgets.QListWidgetItem()
             item.setText(f"自定义-{i}")
-            self.ui.listWidget3.addItem(item)
+            self.ui.listWidget2.addItem(item)
 
     def closeEvent(self, e):
         print("窗口被关闭了")
@@ -41,5 +100,5 @@ class 窗口事件处理(QtWidgets.QWidget):
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    widget = 窗口事件处理()
+    widget = 窗口事件处理()  # 窗口事件处理 使用UI生成的,不要改
     sys.exit(app.exec_())
